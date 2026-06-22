9555e32d9e3637ddaec1f4f23cec85c755de1984
Faizan Butt <bfaizi877@gmail.com>
2026-06-23 01:08:10 +0500

    Configure dynamic PORT for server hosting

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ connectDB();
 
 
 
-app.listen(3000, () => {
-  console.log(`Server running on port 3000`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
